33ea00d43b85adbfd15efe924f265b5652394b9a
Raj Sharma <keyrajsharma86@gmail.com>
2026-08-12 11:15:56 +0530

    new password and id

--- provider.tf
+++ provider.tf
@@ -1,10 +1,13 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
-    azurerm={
-        source = "hashicorp/azurerm"
-        version = "5.0.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.81.0"
     }
   }
+
   backend "azurerm" {
     resource_group_name = "b18g35-common-rg"
     storage_account_name = "b18g35storageaccount"
